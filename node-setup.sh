3b73403df760448103b5c962104cc0a06d9cefc5
Iason Chatzopoulos <jasonjente@gmail.com>
2022-04-21 12:56:40 +0300

    Update node-setup.sh
    minor fix. 
    No need to install npm for docker to run. Docker will download npm in the container.

--- node-setup.sh
+++ node-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-npm init -y
-apt update -y & apt upgrade -y
-apt install npm -y
+#remove comments for installation on the host machine and comment out the docker commands.
+#npm init -y
+#apt update -y & apt upgrade -y
+#apt install npm -y
 docker build . -t helloworld/node-web-app
 docker run -p 49160:8080 -d --name helloworld helloworld/node-web-app
